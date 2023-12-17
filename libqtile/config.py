LCA-CI-builds-repair/qtile

commit 40e97a88371f715fab0b97c45d0421a27267a9bd

Config: Do not match on substrings (#4603)
For some reason in the Match class we match on substrings. So e.g. for
wm_class if value = ["test1", "bogus"] and our candidate is "test1what" we
would loop through the list and determine that the test passes because
test1 is a substring of test1what. This leads to confusion and bugs
such as #4602 and #2670.

In the current situation even the default config is pretty broken on
X11 with the float rules (e.g. wm_type dialo would
match dialog and thus result in a floating window).

libqtile/config.py
@@ -829,7 +829,7 @@ class Match:
     against and returns a boolean.
 
     For some properties, :class:`Match` supports both regular expression objects (i.e.
-    the result of ``re.compile()``) or strings (match as an "include"-match). If a
+    the result of ``re.compile()``) or strings (match as an exact string). If a
     window matches all specified values, it is considered a match.
 
     Parameters
@@ -896,16 +896,14 @@ class Match:
         elif name == "wm_class":
 
             def predicate(other) -> bool:  # type: ignore
-                # match as an "include"-match on any of the received classes
-                match = getattr(other, "match", lambda v: v in other)
+                match = getattr(other, "match", lambda v: v == other)
                 return value and any(match(v) for v in value)
 
             return predicate
         else:
 
             def predicate(other) -> bool:  # type: ignore
-                # match as an "include"-match
-                match = getattr(other, "match", lambda v: v in other)
+                match = getattr(other, "match", lambda v: v == other)
                 return match(value)
 
             return predicate

commit a5b4aaefdbd7ee2a1e055f6f2a73eb3154eb9590

Config: Support lists in Match for backwards compatibility
This was broken in https://github.com/qtile/qtile/pull/4603

IMO we should only support regexes and strings going forwards, like
the docstring already says.

libqtile/config.py
@@ -28,6 +28,7 @@
 from __future__ import annotations
 
 import os.path
+import re
 import sys
 from typing import TYPE_CHECKING
 
@@ -37,7 +38,6 @@ from libqtile.command.base import CommandObject, expose_command
 from libqtile.log_utils import logger
 
 if TYPE_CHECKING:
-    import re
     from typing import Any, Callable, Iterable
 
     from libqtile.backend import base
@@ -818,6 +818,20 @@ class ScratchPad(Group):
         )
 
 
+def convert_deprecated_list(vals: list[str], name: str) -> re.Pattern:
+    # make a regex with OR on word boundaries
+    regex_input = r"^({})$".format("|".join(map(re.escape, vals)))
+    logger.warning(
+        "Your Match with the %s property is using lists which are deprecated, replace Match(%s=%s) with Match(%s=re.compile(r\"%s\")) after importing the 're' module",
+        name,
+        name,
+        vals,
+        name,
+        regex_input,
+    )
+    return re.compile(regex_input)
+
+
 class Match:
     """
     Window properties to compare (match) with a window.
@@ -868,10 +882,18 @@ class Match:
         self._rules: dict[str, Any] = {}
 
         if title is not None:
+            if isinstance(title, list):  # type: ignore
+                title = convert_deprecated_list(title, "title")
             self._rules["title"] = title
         if wm_class is not None:
+            if isinstance(wm_class, list):  # type: ignore
+                wm_class = convert_deprecated_list(wm_class, "wm_class")
             self._rules["wm_class"] = wm_class
         if wm_instance_class is not None:
+            if isinstance(wm_instance_class, list):  # type: ignore
+                wm_instance_class = convert_deprecated_list(
+                    wm_instance_class, "wm_instance_class"
+                )
             self._rules["wm_instance_class"] = wm_instance_class
         if wid is not None:
             self._rules["wid"] = wid
@@ -885,8 +907,12 @@ class Match:
             self._rules["func"] = func
 
         if role is not None:
+            if isinstance(role, list):  # type: ignore
+                role = convert_deprecated_list(role, "role")
             self._rules["role"] = role
         if wm_type is not None:
+            if isinstance(wm_type, list):  # type: ignore
+                wm_type = convert_deprecated_list(wm_type, "wm_type")
             self._rules["wm_type"] = wm_type
 
     @staticmethod