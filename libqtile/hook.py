0d0abf9ec3f27bb3057e751170096999a5ad2ad2
ubuntu <ubuntu@ip-10-0-1-6>
2025-01-08 15:10:30 +0000

    waleko__gpt-4o_44f7__id_172

--- libqtile/hook.py
+++ libqtile/hook.py
@@ -104,7 +104,7 @@ class Hook:
 
 class Subscribe:
     def __init__(self, registry_name: str, check_name=True):
-        self.hooks = set([])
+        self.hooks: set[str] = set()
         if check_name and registry_name in subscriptions:
             raise NameError("A hook registry already exists with that name: {registry_name}")
         elif registry_name not in subscriptions:
